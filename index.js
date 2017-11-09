0b6e3491046b748d5683bbff933dbd74a975e96e
Vincent Baalmans <v.r.baalmans@st.hanze.nl>
2017-11-09 21:46:14 +0100

    Added gambling because fun.

--- index.js
+++ index.js
@@ -2,31 +2,86 @@ var Discord = require('discord.io');
 var logger = require('winston');
 var auth = require('./auth.json');
 
+var guesses = [];
+var number = null;
+
 var bot = new Discord.Client({
-  token: auth.token,
-  autorun: true
+    token: auth.token,
+    autorun: true
 });
 
 bot.on('ready', function (evt) {
-  logger.info('Connected');
-  logger.info('Logged in as: ');
-  logger.info(bot.username + ' - (' + bot.id + ')');
+    logger.info('Connected');
+    logger.info('Logged in as: ');
+    logger.info(bot.username + ' - (' + bot.id + ')');
 });
 
 bot.on('message', function (user, userID, channelID, message, evt) {
-  if (message.substring(0, 1) == '!') {
-    var args = message.substring(1).split(' ');
-    var cmd = args[0];
+    if (message.substring(0, 1) == '!') {
+        var args = message.substring(2).split(' ');
+        var cmd = args[0];
 
-    args = args.splice(1);
-    switch (cmd) {
-      case 'ping':
-        bot.sendMessage({
-          to: channelID,
-          message: 'Pong!'
-        });
-      default:
-        break;
+        switch (cmd) {
+            case 'ping':
+                bot.sendMessage({
+                    to: channelID,
+                    message: 'Pong!'
+                });
+                break;
+            case 'help':
+                bot.sendMessage({
+                    to: channelID,
+                    message: '/tts Type !guess and a number between 0 and 10 to play the guessing game.'
+                });
+                break;
+            case 'guess':
+                if(number == null){
+                    number = Math.floor(Math.random() * 10) + 1
+                }
+                if (args[1] == null) {
+                    bot.sendMessage({
+                        to: channelID,
+                        message: '/tts Please enter a number to play'
+                    });
+                } else {
+                    guesses[user.username] = args[1];
+                    bot.sendMessage({
+                        to: channelID,
+                        message: '/tts ' + user.username + '  has entered' + args[1]
+                    });
+                }
+                break;
+            case 'results':
+                var winners = [];
+                guesses.forEach(function (guess, name) {
+                    if(guess == number){
+                        winners.push(name);
+                    }
+                });
+                number = null;
+                if(winners.length = 0){
+                    bot.sendMessage({
+                        to: channelID,
+                        message: '/tts No one won. You are all idiots..'
+                    });
+                } else {
+                    var message = "/tts The winners are: ";
+                    winners.forEach(function (winner) {
+                       message += winner + " "
+                    });
+                    message += " Yayy!";
+                    bot.sendMessage({
+                        to: channelID,
+                        message: message
+                    });
+                }
+                break;
+            default:
+                break;
+        }
     }
-  }
 });
+
+function guessGame() {
+
+}
