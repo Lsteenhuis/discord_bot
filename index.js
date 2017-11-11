ab889a09794b6745a3686ea66c8d2ad5d709d469
Vincent Baalmans <vincentbaalmans@gmail.com>
2017-11-12 00:54:47 +0100

    nice and clean.

--- index.js
+++ index.js
@@ -27,101 +27,125 @@ client.on('message', function (msg) {
                     msg.reply('Type !guess and a number between 0 and 10 to play the guessing game.');
                     break;
                 case 'guess':
-                    if (toGuess == null) {
-                        toGuess = Math.floor(Math.random() * 10) + 1;
-                    }
-                    if (args[1] == null) {
-                        msg.reply('Please enter a number to play');
-                    } else {
-                        guesses[msg.author.username] = args[1];
-                        if (!isNumber(args[1])) {
-                            msg.reply(msg.author.username + ' Enter a number you asshole');
-                            break;
-                        }
-                        if (args[1] > 10 || args[1] < 0) {
-                            msg.reply(msg.author.username + ' Between 0 and 10 you cunt');
-                            break;
-                        }
-                        msg.reply(msg.author.username + '  has entered ' + args[1]);
-                    }
+                    onGuess(msg, args);
                     break;
                 case 'results':
-                    var winners = [];
-                    for (key in guesses) {
-                        if (parseInt(guesses[key]) == parseInt(toGuess)) {
-                            winners.push(key);
-                        }
-                    }
-                    if (winners.length == 0) {
-                        var winnerNumber;
-                        if (toGuess == null) {
-                            winnerNumber = 'not set';
-                        } else {
-                            winnerNumber = toGuess;
-                        }
-                        msg.reply('No one won. You are all idiots.. The number was ' + winnerNumber);
-                        toGuess = null;
-                    } else {
-                        var winnersMessage = "The winners are: ";
-                        for (key in winners) {
-                            winnersMessage += winners[key] + ", ";
-                            if (gameWinners[winners[key]] == undefined) {
-                                gameWinners[winners[key]] = 1;
-                            } else {
-                                gameWinners[winners[key]] = gameWinners[winners[key]] + 1;
-                            }
-                        }
-                        winnersMessage += "Yay!";
-                        msg.reply(winnersMessage);
-                        toGuess = null;
-                    }
+                    onResults(msg);
                     break;
                 case 'winners':
-                    var winnerMessage = "The winners are: ";
-                    for (guy in gameWinners) {
-                        winnerMessage += guy + " " + gameWinners[guy] + " times,"
-                    }
-                    msg.reply(winnerMessage);
+                    onWinners(msg);
                     break;
                 case 'img':
-                    if (args[1] == null) {
-                        msg.reply('Search for an image by entering keywords after !img');
-                    } else {
-                        var imgSearchTerm = '';
-                        for (var i = 1; i < args.length; i++) {
-                            imgSearchTerm += args[i];
-                        }
-                        imgSearch(imgSearchTerm, msg);
-                    }
+                    img(msg, args);
                     break;
                 case 'gif':
-                    if (args[1] == null) {
-                        msg.reply('Search for an gif by entering keywords after !gif');
-                    } else {
-                        var gifSearchParameter = "";
-                        for (var y = 1; y < args.length; y++) {
-                            gifSearchParameter += args[y];
-                        }
-                        gifSearch(gifSearchParameter, msg);
-                    }
+                    gif(msg, args);
                     break;
                 case 'meaningoflife':
-                    var meaning;
-                    switch (Math.floor(Math.random() * 2) + 1) {
-                        case 1:
-                            meaning = 'BIER!';
-                            break;
-                        case 2:
-                            meaning = '42';
-                            break;
-                    }
-                    msg.reply(meaning);
+                    meaningOfLife(msg);
                     break;
                 default:
                     break;
             }
         }
 });
+
+function onGuess(msg, args) {
+    if (toGuess == null) {
+        toGuess = Math.floor(Math.random() * 10) + 1;
+    }
+    if (args[1] == null) {
+        msg.reply('Please enter a number to play');
+    } else {
+        guesses[msg.author.username] = args[1];
+        if (!isNumber(args[1])) {
+            msg.reply(msg.author.username + ' Enter a number you asshole');
+            return;
+        }
+        if (args[1] > 10 || args[1] < 0) {
+            msg.reply(msg.author.username + ' Between 0 and 10 you cunt');
+            return;
+        }
+        msg.reply(msg.author.username + '  has entered ' + args[1]);
+    }
+}
+
+function onResults(msg) {
+    var winners = [];
+    for (key in guesses) {
+        if (parseInt(guesses[key]) == parseInt(toGuess)) {
+            winners.push(key);
+        }
+    }
+    if (winners.length == 0) {
+        var winnerNumber;
+        if (toGuess == null) {
+            winnerNumber = 'not set';
+        } else {
+            winnerNumber = toGuess;
+        }
+        msg.reply('No one won. You are all idiots.. The number was ' + winnerNumber);
+        toGuess = null;
+    } else {
+        var winnersMessage = "The winners are: ";
+        for (key in winners) {
+            winnersMessage += winners[key] + ", ";
+            if (gameWinners[winners[key]] == undefined) {
+                gameWinners[winners[key]] = 1;
+            } else {
+                gameWinners[winners[key]] = gameWinners[winners[key]] + 1;
+            }
+        }
+        winnersMessage += "Yay!";
+        msg.reply(winnersMessage);
+        toGuess = null;
+    }
+}
+
+function onWinners(msg) {
+    var winnerMessage = "The winners are: ";
+    for (guy in gameWinners) {
+        winnerMessage += guy + " " + gameWinners[guy] + " times,"
+    }
+    msg.reply(winnerMessage);
+}
+
+function img(msg, args) {
+    if (args[1] == null) {
+        msg.reply('Search for an image by entering keywords after !img');
+    } else {
+        var imgSearchTerm = '';
+        for (var i = 1; i < args.length; i++) {
+            imgSearchTerm += args[i];
+        }
+        imgSearch(imgSearchTerm, msg);
+    }
+}
+
+function gif(msg, args) {
+    if (args[1] == null) {
+        msg.reply('Search for an gif by entering keywords after !gif');
+    } else {
+        var gifSearchParameter = "";
+        for (var y = 1; y < args.length; y++) {
+            gifSearchParameter += args[y];
+        }
+        gifSearch(gifSearchParameter, msg);
+    }
+}
+
+function meaningOfLife(msg){
+    var meaning;
+    switch (Math.floor(Math.random() * 2) + 1) {
+        case 1:
+            meaning = 'BIER!';
+            break;
+        case 2:
+            meaning = '42';
+            break;
+    }
+    msg.reply(meaning);
+}
 
 
 function isNumber(obj) {
