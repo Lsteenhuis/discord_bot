245dba3bb712b48ffc6b5caa4c366d3b955d4842
Vincent Baalmans <vincentbaalmans@gmail.com>
2017-11-20 11:57:04 +0100

    BEKFAST. requires ffmpeg to be installed and added to PATH variable.

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
-var logger = require('winston');
-var auth = require('./auth.json');
+const auth = require('./auth.json');
 const GoogleImages = require('google-images');
-var giphy = require('giphy-api')(auth.giphy);
-var request = require('request');
-
-
+const giphy = require('giphy-api')(auth.giphy);
+const request = require('request');
 const Discord = require("discord.js");
 const client = new Discord.Client();
 client.login(auth.discord);
@@ -12,19 +9,19 @@ client.login(auth.discord);
 
 const imgClient = new GoogleImages('014542467886679922052:guxkga1igcq', auth.google);
 
-var guesses = {};
-var toGuess = null;
-var gameWinners = [];
+let guesses = {};
+let toGuess = null;
+let gameWinners = [];
 
 
 client.on('message', function (msg) {
     if (!msg.author.bot)
+        var args = msg.content.substring(1).split(' ');
         if (msg.content.substring(0, 1) == '!') {
-            var args = msg.content.substring(1).split(' ');
-            var cmd = args[0];
+            let cmd = args[0];
             switch (cmd) {
                 case 'help':
-                    msg.reply('Type !guess and a number between 0 and 10 to play the guessing game.');
+                    msg.tts('Type !guess and a number between 0 and 10 to play the guessing game.');
                     break;
                 case 'guess':
                     onGuess(msg, args);
@@ -47,18 +44,37 @@ client.on('message', function (msg) {
                 case  'joke':
                     onJoke(msg);
                     break;
+                case 'bekfast':
+                    onBekfast(msg);
+                    break;
                 default:
                     break;
             }
+        } else {
+            annoyingResponse(msg);
         }
 });
+
+function onBekfast(msg){
+    playAudioFile(msg, './bekfast.mp3');
+}
+
+function annoyingResponse(msg) {
+    var args = msg.content.split(' ');
+    if(args.includes('marcel')){
+        msg.tts("ma ma ma ma ma marcell");
+    }
+    if(args.includes('ontbijt' ) || args.includes('breakfast') || args.includes('avondeten')){
+
+    }
+}
 
 function onJoke(msg) {
     request('http://www.reddit.com/r/jokes/top/.json', function (error, response, body) {
         if (!error && response.statusCode == 200) {
-            var json = JSON.parse(body);
-            var randomNumber = Math.floor(Math.random() * 25);
-            var joke = json.data.children[randomNumber];
+            let json = JSON.parse(body);
+            let randomNumber = Math.floor(Math.random() * 25);
+            let joke = json.data.children[randomNumber];
             msg.reply(joke.data.title + ": \n\n" + joke.data.selftext);
         }
     })
@@ -85,7 +101,7 @@ function onGuess(msg, args) {
 }
 
 function onResults(msg) {
-    var winners = [];
+    let winners = [];
     for (key in guesses) {
         if (parseInt(guesses[key]) == parseInt(toGuess)) {
             winners.push(key);
@@ -101,7 +117,7 @@ function onResults(msg) {
         msg.reply('No one won. You are all idiots.. The number was ' + winnerNumber);
         toGuess = null;
     } else {
-        var winnersMessage = "The winners are: ";
+        let winnersMessage = "The winners are: ";
         for (key in winners) {
             winnersMessage += winners[key] + ", ";
             if (gameWinners[winners[key]] == undefined) {
@@ -117,7 +133,7 @@ function onResults(msg) {
 }
 
 function onWinners(msg) {
-    var winnerMessage = "The winners are: ";
+    let winnerMessage = "The winners are: ";
     for (guy in gameWinners) {
         winnerMessage += guy + " " + gameWinners[guy] + " times,"
     }
@@ -128,8 +144,8 @@ function img(msg, args) {
     if (args[1] == null) {
         msg.reply('Search for an image by entering keywords after !img');
     } else {
-        var imgSearchTerm = '';
-        for (var i = 1; i < args.length; i++) {
+        let imgSearchTerm = '';
+        for (let i = 1; i < args.length; i++) {
             imgSearchTerm += args[i];
         }
         imgSearch(imgSearchTerm, msg);
@@ -140,8 +156,8 @@ function gif(msg, args) {
     if (args[1] == null) {
         msg.reply('Search for an gif by entering keywords after !gif');
     } else {
-        var gifSearchParameter = "";
-        for (var y = 1; y < args.length; y++) {
+        let gifSearchParameter = "";
+        for (let y = 1; y < args.length; y++) {
             gifSearchParameter += args[y];
         }
         gifSearch(gifSearchParameter, msg);
@@ -149,7 +165,7 @@ function gif(msg, args) {
 }
 
 function meaningOfLife(msg) {
-    var meaning;
+    let meaning;
     switch (Math.floor(Math.random() * 2) + 1) {
         case 1:
             meaning = 'BIER!';
@@ -186,5 +202,71 @@ function gifSearch(searchParam, msg) {
         } else {
             msg.reply(res.data[0].url);
         }
+    });
+}
+
+function playAudioFile(message, file) {
+    let textChannel = message.channel;
+    let guild = message.guild;
+
+    // make sure the text channel is a guild channel (type = text)
+    if (textChannel.type != "text") {
+        textChannel.send("I can't be invoked in private messages, only in guilds.");
+        return;
+    }
+    // default, will be overwritten by argument if needed
+    let voiceChannel = message.member.voiceChannel;
+    let play = true;
+
+    let isBusy = isBusyInGuild(guild);
+
+    if (isBusy) {
+        textChannel.send("I am currently needed in Channel '" + isBusy.name + "'.");
+        play = false;
+    }
+
+    if (play){
+        if (voiceChannel) {
+            playAudio(voiceChannel, file, textChannel);
+        } else {
+            textChannel.send("You have to be in a voice channel to do this.");
+        }
+    }
+}
+
+function isBusyInGuild(guild) {
+
+    let connections = Array.from(client.voiceConnections.values());
+
+    for (let i = 0; i < connections.length; i++) {
+        let connection = connections[i];
+
+        if (connection.channel.guild == guild) {
+            return connection.channel;
+        }
+    }
+    return false;
+}
+
+function playAudio(voiceChannel, file, textChannel) {
+    // check for permissions first
+    if (!voiceChannel.permissionsFor(client.user.id).has("CONNECT")) {
+        textChannel.send("No permission to join this channel.");
+        return;
+    }
+    if (!voiceChannel.permissionsFor(client.user.id).has("SPEAK")) {
+        textChannel.send("No permission to speak in this channel.");
+        return;
+    }
+
+    voiceChannel.join().then(connection => {
+
+        connection.playFile(file).on("end", () => {
+            connection.disconnect();
+            voiceChannel.leave();
+        });
+
+    }).catch(error => {
+        textChannel.send(error.toString());
     });
 }
